663ae77c5e59f94b70be9410c09953940f3e0060
Pooja patel <poojavpatel71@gmail.com>
2018-07-22 18:42:34 +0530

    js all done

--- script.js
+++ script.js
@@ -8,12 +8,16 @@ function setDate() {
    //console.log(now);
    const seconds = now.getSeconds();
    const minutes = now.getMinutes();
-   const hours = now.getHours();
+   let hours = now.getHours();
+   if (hours > 12) {
+       hours = hours - 12;
+   }
    console.log(` ${hours}hr ${minutes}min ${seconds}sec`);
    // 0s is 0deg , 30s is 180deg , 60s is 360deg
+   // 1hr 30deg ,3hr 90deg , 6hr 180deg, 12hr 0deg 
    const secondDegrees = ((seconds/60) * 360) - 90 ;
    const minuteDegrees = ((minutes/60) * 360) - 90 ;
-   const hourDegrees = ((hours/60) * 360) - 90 ;
+   const hourDegrees = ((hours/12) * 360) - 90 ;
    //console.log(secondDegrees);
    secondHand.style.transform = `rotate(${secondDegrees}deg)` ;
    minuteHand.style.transform = `rotate(${minuteDegrees}deg)` ;
